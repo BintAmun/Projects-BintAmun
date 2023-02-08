893b4ca1046e45dd96a121cad73fd8207eb17711
Ashley <64813439+BintAmun@users.noreply.github.com>
2023-02-08 10:18:13 -0500

    New project

---
+++ Routine.java
@@ -0,0 +1,13 @@
+/**
+ * Routine
+ */
+public class Routine {
+
+    public static void main(String[] args) {
+        System.out.println("G'day! This program will determine whether you get to have any enjoyment in your downtime.");
+        System.out.println("Your level of fun during the tiny bit of recreational time you have in your day will be proportional to how productive you were each day.");
+        System.out.println("I will now determine your fate! I hope you are ready to experience the consequences of your actions!");
+
+        
+    }
+}
